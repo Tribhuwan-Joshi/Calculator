0e5ef3c1b853e46d7af817ca97ab427cb66f2417
Tribhuwan <mathincosmos@gmail.com>
2022-04-29 13:09:58 +0530

    Add calculator's display

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
 /* Math functions for operations */
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
- 
+
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    let res = a/b;
-    return (Number.isInteger(res) ? res : res.toFixed(2));
+  let res = a / b;
+  return Number.isInteger(res) ? res : res.toFixed(2);
 }
 
-
-function pow(a,b) {
-    return Math.pow(a, b);
+function pow(a, b) {
+  return Math.pow(a, b);
 }
-
 
 /* operate will call math functions */
 
 function operate(operater, a, b) {
-    switch (operater) {
-        case '+':
-            add(a, b);
-            break;
-        case '-':
-            subtract(a, b);
-            break;
-        case '*':
-            multiply(a, b);
-            break;
-        case '/':
-            divide(a, b);
-            break;
-        case '^':
-            pow(a, b);
-            break;
-        
-            
-    }
+  switch (operater) {
+    case "+":
+      return add(a, b);
+
+    case "-":
+      subtract(a, b);
+          return add(a, b);
+      
+    case "*":
+      multiply(a, b);
+          return add(a, b);
+      
+    case "/":
+      divide(a, b);
+          return add(a, b);
+      
+    case "^":
+      pow(a, b);
+          return add(a, b);
+      
+  }
 }
-
-
